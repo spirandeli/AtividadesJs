3844f0c512217d9a10aaf10bdcbc3fe03f73d46b
spirandeli <spirandelidev@gmail.com>
2022-03-10 16:58:30 -0300

    Create ex18.js

---
+++ ex18.js
@@ -0,0 +1,31 @@
+function escrevaonumero(numero){
+    switch(numero){
+        case 1:
+            return nome = ("UM")
+        case 2:
+            return nome = ("DOIS")
+        case 3:
+            return nome = ("TRES")
+        case 4:
+            return nome = ("QUATRO")
+        case 5:
+            return nome = ("CINCO")
+        case 6:
+            return nome = ("SEIS")
+        case 7:
+            return nome = ("SETE")
+        case 8:
+            return nome = ("OITO") 
+        case 9:
+            return nome = ("NOVE")
+        case 10:
+            return nome = ("DEZ")           
+        default:
+            return ("NUMERO INVALIDO")
+    }
+}
+
+
+
+
+console.log(escrevaonumero(4))
